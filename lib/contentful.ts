1c5e491ccb09c77ad169394a8edff97015c59fe6
signals-web <chris.grimley@gmail.com>
2025-04-03 12:51:42 -0400

    fix: export Project and ProjectFields types for components

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -1,24 +1,28 @@
 import { createClient, Entry } from 'contentful'
-import { ProjectSkeleton } from './contentful-types'
+import { ProjectSkeleton, IProjectFields } from './contentful-types'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID!,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
 })
 
-export async function getProjects(): Promise<Entry<ProjectSkeleton>[]> {
+// Export the types that other components need
+export type Project = Entry<ProjectSkeleton>
+export type ProjectFields = IProjectFields
+
+export async function getProjects(): Promise<Project[]> {
   const response = await client.getEntries({
     content_type: 'project'
   })
 
-  return response.items as Entry<ProjectSkeleton>[];
+  return response.items as Project[]
 }
 
-export async function getProjectsByType(type: 'Book' | 'Sign'): Promise<Entry<ProjectSkeleton>[]> {
+export async function getProjectsByType(type: 'Book' | 'Sign'): Promise<Project[]> {
   const response = await client.getEntries({
     content_type: 'project',
     'fields.type': type
   })
 
-  return response.items as Entry<ProjectSkeleton>[];
+  return response.items as Project[]
 }
