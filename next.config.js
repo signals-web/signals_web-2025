7cedc852202162110340f757ae8f3f850aa642f7
signals-web <chris.grimley@gmail.com>
2025-04-05 09:03:47 -0400

    feat: add www to non-www redirect and security headers

--- next.config.js
+++ next.config.js
@@ -9,6 +9,46 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.sendoutsignals.com',
+          },
+        ],
+        destination: 'https://sendoutsignals.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+  headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig 
